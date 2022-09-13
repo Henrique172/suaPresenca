710e2aa318c4f28417bbe7723fd6855965dfefc2
Henrique172 <44645433+Henrique172@users.noreply.github.com>
2022-09-13 11:30:54 -0300

    lista de presenca pdf1

--- resources/views/listaPresenca/listaPdf.blade.php
+++ resources/views/listaPresenca/listaPdf.blade.php
@@ -3,10 +3,10 @@
 @section('title', 'Lista de Chamada')
 
 <?php
-
+use DateTime;
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
 date_default_timezone_set('America/Sao_Paulo');
-$data = new dateTime(); 
+$data = new DateTime(); 
 
 ?>
 
