bffdd5446509df336a1781e40e95aec3cfe81bf0
Henrique172 <44645433+Henrique172@users.noreply.github.com>
2022-09-22 23:47:14 -0300

    COntando quantidade de criancas soma 1.11

--- resources/views/membros/index.blade.php
+++ resources/views/membros/index.blade.php
@@ -11,7 +11,7 @@
   </div>
   @endauth
 <div class="col-md-12">
- <b> Total de Membros:</b> {{ count($find) }}  <b style="padding-left: 80px;"> Qnts de Crianças:</b> {{ $qntdCrianca }}
+ <b> Total de Membros:</b> {{ count($find) - $qntdCrianca }}  <b style="padding-left: 80px;"> Qnts de Crianças:</b> {{ $qntdCrianca }}
 </div>
 
 <table class="table col-md-12" id="tableMembro">
